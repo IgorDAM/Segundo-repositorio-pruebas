7f437fd9fb93615290792f960f009ed93c1fd0f3
flowt <flowt@pcigor>
2023-11-28 22:56:07 +0100

    1ªsemana de trabajo

--- PruebaGit/src/main/java/principal/Principal.java
+++ PruebaGit/src/main/java/principal/Principal.java
@@ -4,7 +4,10 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Comienzo de mi programa dificilisimo");
+		System.out.println("Sigo con mi programa dificilisimo");
+		System.out.println("Añado funcionalidades en mi programa dificilisimo");
+		System.out.println("Estoy muy contento con mi programa dificilisimo");
 	}
 
 }
