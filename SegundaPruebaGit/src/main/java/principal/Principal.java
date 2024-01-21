43f4d97778922c5b3bd7ee9b7b717ae78f14e34f
flowt <flowt@pcigor>
2024-01-21 17:45:54 +0100

    Editada la clase principal

--- SegundaPruebaGit/src/main/java/principal/Principal.java
+++ SegundaPruebaGit/src/main/java/principal/Principal.java
@@ -5,6 +5,11 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		System.out.println("Prog.Dificil");
+		System.out.println("Sigo con el prog dificil");
+		System.out.println("Añado funcionalidades");
+		System.out.println("Estoy muy contento con el programa");
 	}
 
 }
