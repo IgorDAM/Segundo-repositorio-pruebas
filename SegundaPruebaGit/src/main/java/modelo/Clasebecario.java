f532d84b8969e605a541f8c7eb232644a92addc9
flowt <flowt@pcigor>
2024-01-22 19:26:44 +0100

    segundo commit en becario2

--- SegundaPruebaGit/src/main/java/modelo/Clasebecario.java
+++ SegundaPruebaGit/src/main/java/modelo/Clasebecario.java
@@ -5,6 +5,7 @@ public class Clasebecario {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Trabajo duro");
+		System.out.println("segundo dia Trabajo duro");
 	}
 
 }
